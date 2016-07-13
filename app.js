5b4ccba34c00c3ce173391abe8b95cf5a2a56de7
Cameron White <camw85@mac.com>
2016-07-13 16:44:49 -0700

    :hammer: setting up oembed

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const path = require('path');
+const connect = require('connect');
 const express = require('express');
+const oembed = require('connect-oembed');
+const EmbedUtils = require('./utils/EmbedUtils');
 
 let app = express();
 let pathToPlayer = path.join(__dirname, 'player/player.html');
 
 app.use('/player', express.static(pathToPlayer));
-
-app.get('/', (req, res) => {
+app.get('/player', (req, res) => {
   res.sendFile(pathToPlayer);
-})
+});
+
+app.use('/oembed', oembed((req, res, next) => {
+  let iframe = EmbedUtils.buildIframe();
+  res.oembed.rich(iframe, 100, 400);
+}));
 
 module.exports = app;
---
+++ utils/EmbedUtils.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class EmbedUtils {
+  static buildIframe() {
+    let html = '<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://562ba361.ngrok.io/player"></iframe>'
+
+    return html;
+  }
+}
+
+module.exports = EmbedUtils;
